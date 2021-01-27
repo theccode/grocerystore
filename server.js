3353ef4de99fd6c0e4b905091b862df27b4ca786
Eric Kumah <theccode@Erics-MacBook-Pro.local>
2021-01-27 09:13:40 +0000

    graphql queries entered

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const jsonServer = require("json-server");
 const chokidar = require("chokidar");
 const cors = require("cors");
-const { json } = require("express");
+const fs = require("fs");
+const { buildSchema }  = require("graphql");
+const graphqlHTTP = require("express-graphql");
+const queryResolvers = require("./serverQueriesResolver");
+const mutationResolvers = require("./serverMutationsResolver");
 
 const fileName = process.argv[2] || "./data.js";
 const port = process.argv[3] || 3500;
 let router = undefined;
+let graph = undefined;
 
 const app = express();
 
@@ -14,12 +19,18 @@ const createServer = () => {
     delete require.cache[require.resolve[fileName]];
     setTimeout(()=>{
         router = jsonServer.router(fileName.endsWith(".js") ? require(fileName)() : fileName)
+        let schema = fs.readFileSync("./serverQueriesSchema.graphql", "utf-8") + fs.readFileSync("./serverMutationsSchema.graphql", "utf-8");
+        let resolvers = { ...queryResolvers, ...mutationResolvers};
+        graph = graphqlHTTP({
+            schema: buildSchema(schema),rootValue: resolvers, graphiql: true, context: {db: router.db}
+        })
     }, 100)
 }
 createServer();
 app.use(cors());
-app.use(jsonServer.bodyParser)
-app.use("/api", (req, resp, next)=> router(req, resp, next));
+app.use(jsonServer.bodyParser);
+app.use("/api", (req, resp, next)=> router(req, resp, next));//localhost:3000/api
+app.use("/graphql", (req, resp, next) => graph(req, resp, next));//localhost:3500/graphql
 
 chokidar.watch(fileName).on("change", ()=>{
     console.log("Reloading Web service data...");
---
+++ serverQueriesResolver.js
@@ -0,0 +1,40 @@
+const paginateQuery = (query, page = 1, pageSize = 5) => query.drop((page-1) * pageSize).take(pageSize);
+
+const product = ({id}, {db}) => db.get("products").getById(id).value();
+
+const products = ({ category }, { db }) => ({
+    totalSize: () => db.get("products").filter(p => category ? new RegExp(category, "i").test(p.category) : p).size().value(),
+    products: ({page, pageSize, sort}) => {
+        let query = db.get("products");
+        if (category){
+            query = query.filter(item => new RegExp(category, "i").test(item.category))
+        }
+        if (sort) { query = query.orderBy(sort)}
+        return paginateQuery(query, page, pageSize).value();
+    }
+})
+
+const categories = (args, {db}) => db.get("categories").value();
+
+const resolveProducts = (product, db) => products.map(p => ({
+    quantity: p.quantity, 
+    product: product({id: p.product_id}, {db})
+}))
+
+const resolveOrders = (onlyUnshipped, {page, pageSize, sort}, {db}) => {
+    let query = db.get("orders");
+
+    if (onlyUnshipped) {query = query.filter({shipped: false})}
+    if (sort) { query = query.orderBy(sort)}
+
+    return paginateQuery(query, page, pageSize).value().map(order => ({
+        ...order, products: () => resolveProducts(order.products, db)
+    }));
+}
+
+const orders = ({onlyUnshiped = false}, {db}) => ({
+    totalSize: () => db.get("orders").filter(o => onlyUnshipped ? o.shipped === false: o).size().value(),
+    orders: (...args) => resolveOrders(onlyUnshiped, ...args)
+})
+
+module.exports = {product, products, categories, orders}
